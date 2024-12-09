ee0c0c0166e20857a2ab8ed61c2b12715fb184d8
Iris <iris.van.willigen@gmail.com>
2024-12-09 11:17:05 +0100

    Add interaction: front-end flip

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,8 +1,19 @@
-let interaction = document.querySelector('a:nth-of-type(12)')
+let interaction12 = document.querySelector('a:nth-of-type(12)')
 
-interaction.addEventListener('click', jumpHandler)
-interaction.addEventListener('animationend', jumpHandler)
+interaction12.addEventListener('click', jumpHandler)
+interaction12.addEventListener('animationend', jumpHandler)
 
 function jumpHandler() {
-  interaction.classList.toggle('jump')
+  interaction12.classList.toggle('jump')
 }
+
+//  -----------------------
+let interaction1 = document.querySelector('a:nth-of-type(1)');
+
+interaction1.addEventListener('mouseenter', () => {
+  interaction1.classList.toggle('flip');
+});
+
+interaction1.addEventListener('mouseleave', () => {
+  interaction1.classList.toggle('flip');
+});
